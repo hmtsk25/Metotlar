9861131ca41dcb84fac337b4002267a02e5e7efa
1n0 <19034hm@gmail.com>
2021-01-17 15:48:52 +0300

    16 Ocak Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -11,6 +11,8 @@ namespace DegerVeReferansTipler
             sayi1 = sayi2;
             sayi2 = 65;
             Console.WriteLine(sayi1);
+            Console.WriteLine(sayi2);
+
             
         }
     }
